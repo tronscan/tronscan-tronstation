6d47b8f5e7a3d2845cf35ba052a8288bcb7ef190
Richard <codewellliu@gmail.com>
2022-07-28 18:31:35 +0800

    Initial commit

---
+++ test/helpers/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    SHASTA: {
+        PRIVATE_KEY: 'your private key',
+        HOST: 'https://api.shasta.trongrid.io'
+    },
+
+    LOCAL: {
+        PRIVATE_KEY: 'your private key',
+        HOST: 'http://127.0.0.1:8060'
+    },
+
+    NET: 'shasta',
+
+};
